ea0550d6ba22961f0057f58575862398b05238a5
Ryan Nazareth <ryankarlos@gmail.com>
2021-09-26 19:34:38 +0100

    add repo skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experimenting with different networksx algos and embedding techniques',
+    author='Ryan Nazareth',
+    license='',
+)
