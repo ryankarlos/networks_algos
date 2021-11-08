093d672c86645784175849ab7da99d7751fb1104
Ryan Nazareth <ryankarlos@gmail.com>
2021-11-08 00:29:57 +0000

    add plot functions and run black

--- visualize.py
+++ visualize.py
@@ -6,38 +6,43 @@ from pyvis.network import Network
 from sklearn.decomposition import PCA
 
 
-def draw_network(G, ax, edge_list=None, color="red"):
-    if edge_list is not None:
-        G = nx.Graph()
-        G.add_edges_from(edge_list)
-        pos = nx.spring_layout(G)
-        nx.draw_networkx(G, pos=pos, node_color=color, ax=ax)
-        print(nx.info(G))
-        return G
-    else:
-        nx.draw_networkx(G, node_color=color)
-        print(nx.info(G))
-        return G
+def plot_network_with_edge_weights(G, figsize=(10, 10)):
+    elarge = [(u, v) for (u, v, d) in G.edges(data=True) if (d["weight"] > 0.8)]
+    emedium = [
+        (u, v) for (u, v, d) in G.edges(data=True) if (0.8 >= d["weight"] >= 0.5)
+    ]
+    esmall = [(u, v) for (u, v, d) in G.edges(data=True) if (d["weight"] < 0.5)]
+    plt.figure(figsize=figsize)
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, node_color="red", node_size=300)
+    nx.draw_networkx_edges(G, pos, edgelist=elarge, width=8, alpha=0.2)
+    nx.draw_networkx_edges(G, pos, edgelist=emedium, width=5, alpha=0.2)
+    nx.draw_networkx_edges(G, pos, edgelist=esmall, width=2, alpha=0.2)
+    nx.draw_networkx_labels(
+        G,
+        pos,
+        font_size=10,
+        font_weight="bold",
+        font_family="sans-serif",
+        font_color="white",
+    )
+    plt.axis("off")
+    plt.show()
 
 
-def plot_subgraph(G, node_list, ax):
-    subgraph = G.subgraph(node_list)
-    diameter = nx.diameter(subgraph)
-    print("Network diameter of largest component:", diameter)
-    draw_network(subgraph, color="red", ax=ax)
-    return subgraph
-
-
-def plot_ego_network(G, n, radius, ax):
+def plot_ego_network(G, n, radius, **options):
     """
     plot ego network around a node n depending
     on radius setting i.e. only include upto
     n nodes directly or indirectly connected to
     this node
     """
-    ego_nx = ego_graph(G, n, radius=radius)
-    draw_network(ego_nx, ax=ax, color="red")
-    return ego_nx
+    hub_ego = ego_graph(G, n, radius=radius)
+    pos = nx.spring_layout(hub_ego)
+    nx.draw(hub_ego, pos, node_color="b", node_size=50, with_labels=False)
+    nx.draw_networkx_nodes(hub_ego, pos, nodelist=[n], **options)
+    plt.show()
+    return hub_ego
 
 
 def plot_centrality_hist(centrality, name):
@@ -50,13 +55,21 @@ def plot_centrality_hist(centrality, name):
     plt.show()
 
 
-def plot_interactive_network(model, title):
-    net = Network(notebook=True)
-    net.from_nx(model)
-    net.show_buttons(filter=["physics"])
-    filename = f"{title}.html"
-    net.show(filename)
-    return net
+def interactive_network_vis(
+    dag, *widgets, options=None, weights=False, notebook=True, directed=True
+):
+    nt = Network("800px", "800px", directed=directed, notebook=notebook)
+
+    nt.from_nx(dag)
+    if weights:
+        for edge in nt.edges:
+            edge["value"] = edge["weight"]
+        if options is not None:
+            nt.set_options(options=options)
+            return nt
+        else:
+            nt.show_buttons(filter=widgets)
+            return nt
 
 
 def plot_community_class_count(communities):
@@ -99,4 +112,15 @@ def plot_shortest_paths_hist(frequencies):
     )
     plt.xlabel("Shortest Path Length", fontdict={"size": 22})
     plt.ylabel("Percentage", fontdict={"size": 22})
+    plt.show()
+
+
+def plot_degree_freq_log_log(G, m=0):
+    degree_freq = G.degree_historgam(G)
+    degrees = range(len(degree_freq))
+    plt.figure(figsize=(10, 6))
+    plt.loglog(degrees[m:], degree_freq[m:], "go-")
+    plt.title("log log plot for degree freq")
+    plt.xlabel("degree")
+    plt.ylabel("frequency")
     plt.show()
